9e5c51809239a0b119c4e80deee69fc1f71a42d1
C-o-d-e-r-B-o-y <105557842+C-o-d-e-r-B-o-y@users.noreply.github.com>
2022-05-18 21:43:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-7
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+    background("white");
+}
+
+function draw(){
+
+}
+
+function clearCanvas(){
+    background("white");
+}
